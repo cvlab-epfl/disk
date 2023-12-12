33b590210f314a7e4963551a495ffc49c3cc1bc4
Michał Tyszkiewicz <michal.tyszkiewicz@gmail.com>
2023-12-12 19:14:43 +0100

    Make kornia visualization work

--- src/disk/common/vis_kornia.py
+++ src/disk/common/vis_kornia.py
@@ -1,46 +1,44 @@
 import matplotlib.pyplot as plt
 import kornia.feature as KF
 import kornia as K
+from kornia_moons.viz import draw_LAF_matches
 from disk.common.structs import Features
 from disk.common.image import Image
 from disk.geom.epi import p_asymmdist_from_imgs
-from kornia_moons.viz import draw_LAF_matches
 
+def visualize(features: list[Features], images: list[Image], threshold_px: float = 2.5) -> plt.Figure:
+    assert len(features) == 2
+    assert len(images) == 2
 
-def visualize(features, images) -> plt.Figure:
-    assert len(features) == 1
-    assert len(images) == 1
-
-    features = features[0]
-    images = images[0]
-
-    f1: Features = features[0]
-    f2: Features = features[1]
-    im1: Image = images[0]
-    im2: Image = images[1]
+    f1, f2 = features
+    im1, im2 = images
 
     _dists, idxs = KF.match_smnn(f1.desc, f2.desc, 0.95)
 
     mkpts1 = f1.kp[idxs[:, 0]]
-    mkpts2 = f1.kp[idxs[:, 1]]
+    mkpts2 = f2.kp[idxs[:, 1]]
 
-    asymmdist = p_asymmdist_from_imgs(mkpts1.T, mkpts2.T, im1, im2)
-    is_good = asymmdist < 5
+    asymmdist_ab = p_asymmdist_from_imgs(mkpts1.T, mkpts2.T, im1, im2).abs()
+    asymmdist_ba = p_asymmdist_from_imgs(mkpts2.T, mkpts1.T, im2, im1).abs()
+    is_good = (asymmdist_ab < threshold_px) & (asymmdist_ba < threshold_px)
 
-    fig, ax = draw_LAF_matches(
+    fig, ax = plt.subplots(figsize=(20, 10), constrained_layout=True)
+
+    draw_LAF_matches(
         KF.laf_from_center_scale_ori(f1.kp[None].cpu()),
         KF.laf_from_center_scale_ori(f2.kp[None].cpu()),
         idxs.cpu(),
         K.tensor_to_image(im1.bitmap.cpu()),
         K.tensor_to_image(im2.bitmap.cpu()),
-        is_good,
+        is_good.cpu(),
         draw_dict={
             "inlier_color": (0.2, 1, 0.2),
             "tentative_color": (1, 1, 0.2, 0.3),
             "feature_color": None,
             "vertical": False,
         },
-        return_fig_ax=True,
+        fig=fig,
+        ax=ax,
     )
 
     ax.axis("off")
